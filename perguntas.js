ee0d8395d9084a5958fb5a0e613542599f4c0028
04-ACES <00001072557198sp@al.educacao.sp.gov.br>
2024-11-12 23:11:10 -0300

    tentativa I

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Ceará?',
+    'A capital do Ceará é Fortaleza'
+)
+
+criaCartao(
+    'História',
+    'O que é um Pau de arará?',
+    'É um caminhão comum no nordeste usado como carona' 
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Mandacaru?',
+    'É uma planta mais conhecida como cacto' 
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
